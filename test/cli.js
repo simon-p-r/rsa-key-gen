fc568cb2e5c4e2c3f54429bb9b985de826d03c01
simon-p-r <simon.ricaldone@circabs.com>
2018-04-29 20:39:41 +0100

    v0.6.0 - bumped deps and added appveyor ci

--- test/cli.js
+++ test/cli.js
@@ -1,15 +1,35 @@
 'use strict';
 
 const Code = require('code');
+const Fs = require('fs');
 const Lab = require('lab');
 const Os = require('os');
 const Path = require('path');
 const Cli = require('../lib/cli');
 const RSA = require('../lib/index');
-const Utils = require('basic-utils');
+
 
 // Declare internals
 const internals = {};
+
+
+const rmDir = (dir) => {
+
+    const s = Fs.lstatSync(dir);
+    if (s.isFile()) {
+        Fs.unlinkSync(dir);
+    }
+    if (!s.isDirectory()) {
+        return;
+    }
+
+    const fileArr = Fs.readdirSync(dir);
+    for (const f in fileArr) {
+        rmDir(dir + '/' + fileArr[f]);
+    }
+
+    Fs.rmdirSync(dir);
+};
 
 
 // Test shortcuts
@@ -23,111 +43,144 @@ describe('Cli', () => {
 
     const keysDir = `${Os.tmpdir()}${Path.sep}keys`;
     const rootDir = `${Os.tmpdir()}${Path.sep}keys${Path.sep}root`;
-    lab.before((done) => {
+    lab.before(() => {
 
-        Utils.mkDirSync(keysDir);
-        Utils.mkDirSync(rootDir);
-        done();
+        try {
+            Fs.mkdirSync(keysDir);
+            Fs.mkdirSync(rootDir);
+        }
+        catch (e) {
+
+        }
+
+
     });
 
-    lab.after((done) => {
+    lab.after(() => {
 
-        Utils.rmDirSync(keysDir);
-        Utils.rmDirSync(rootDir);
-        done();
+        try {
+            rmDir(keysDir);
+            rmDir(rootDir);
+        }
+        catch (e) {
+
+        }
     });
 
-    it('runs fails to run command due to invalid args', (done) => {
+    it('runs fails to run command due to invalid args', () => {
 
         const errArgs = ['-invalid', 'args'];
-        Cli.run(errArgs, (err, output) => {
+        return new Promise((resolve) => {
 
-            expect(err).to.exist();
-            expect(output).to.not.exist();
-            done();
+            Cli.run(errArgs, (err, output) => {
+
+                expect(err).to.exist();
+                expect(output).to.not.exist();
+                resolve();
+            });
         });
     });
 
-    it('should show help when -h flag is called', (done) => {
+    it('should show help when -h flag is called', () => {
 
         const helpArgs = ['-h'];
-        Cli.run(helpArgs, (err, output) => {
+        return new Promise((resolve) => {
 
-            expect(err).to.not.exist();
-            expect(output).to.exist();
-            done();
+            Cli.run(helpArgs, (err, output) => {
+
+                expect(err).to.not.exist();
+                expect(output).to.exist();
+                resolve();
+            });
         });
     });
 
-    it('should show version of project when -v flag is called', (done) => {
+    it('should show version of project when -v flag is called', () => {
 
         const versionArgs = ['-v'];
-        Cli.run(versionArgs, (err, output) => {
+        return new Promise((resolve) => {
 
-            expect(err).to.not.exist();
-            expect(output).to.contain('v');
-            done();
+            Cli.run(versionArgs, (err, output) => {
+
+                expect(err).to.not.exist();
+                expect(output).to.contain('v');
+                resolve();
+            });
         });
     });
 
-    it('should return an error when keyPairs cannot be generated due to invalid openssl path', (done) => {
+    it('should return an error when keyPairs cannot be generated due to invalid openssl path', () => {
 
         RSA.setOpenSSLPath('invalid');
         const args = ['-b', '2048'];
-        Cli.run(args, (err, output) => {
+        return new Promise((resolve) => {
 
-            expect(err).to.exist();
-            expect(err.message).to.contain('Could not find openssl');
-            expect(output).to.not.exist();
-            RSA.setOpenSSLPath('openssl');
-            done();
+            Cli.run(args, (err, output) => {
+
+                expect(err).to.exist();
+                expect(err.message).to.contain('Could not find openssl');
+                expect(output).to.not.exist();
+                RSA.setOpenSSLPath('openssl');
+                resolve();
+            });
         });
     });
 
-    it('should generate KeyPairs and save to a specified directory', (done) => {
+    it('should generate KeyPairs and save to a specified directory', () => {
 
         const args = ['-d', keysDir];
-        Cli.run(args, (err, output) => {
+        return new Promise((resolve) => {
 
-            expect(err).to.not.exist();
-            expect(output).to.exist();
-            done();
+            Cli.run(args, (err, output) => {
+
+                expect(err).to.not.exist();
+                expect(output).to.exist();
+                resolve();
+            });
         });
     });
 
-    it('should generate KeyPairs and using a root plus directory flags', (done) => {
+    it('should generate KeyPairs and using a root plus directory flags', () => {
 
         const args = ['-r', rootDir, '-d', './'];
-        Cli.run(args, (err, output) => {
+        return new Promise((resolve) => {
 
-            expect(err).to.not.exist();
-            expect(output).to.exist();
-            done();
+            Cli.run(args, (err, output) => {
+
+                expect(err).to.not.exist();
+                expect(output).to.exist();
+                resolve();
+
+            });
         });
     });
 
 
-    it('should fail to save generated KeyPairs due to an invalid directory', (done) => {
+    it('should fail to save generated KeyPairs due to an invalid directory', () => {
 
         const args = ['-d', 'invalid/directory'];
-        Cli.run(args, (err, output) => {
+        return new Promise((resolve) => {
 
-            expect(err).to.exist();
-            expect(output).to.not.exist();
-            done();
+            Cli.run(args, (err, output) => {
+
+                expect(err).to.exist();
+                expect(output).to.not.exist();
+                resolve();
+            });
         });
     });
 
-    it('should generate keyPairs and print via stdout', (done) => {
+    it('should generate keyPairs and print via stdout', () => {
 
         const args = [];
-        Cli.run(args, (err, output) => {
+        return new Promise((resolve) => {
 
-            expect(err).to.not.exist();
-            expect(output).to.exist();
-            done();
+            Cli.run(args, (err, output) => {
+
+                expect(err).to.not.exist();
+                expect(output).to.exist();
+                resolve();
+            });
         });
     });
-
-
 });
--- test/index.js
+++ test/index.js
@@ -13,119 +13,138 @@ const expect = Code.expect;
 
 describe('Key-Gen', () => {
 
-    it('should throw an error when generateKeyPair function has no callback', (done) => {
+    it('should throw an error when generateKeyPair function has no callback', () => {
 
         expect(() => Gen.generateKeyPair()).throws(Error);
-        done();
     });
 
-    it('should return an error from generateKeyPair due to invalid options', (done) => {
+    it('should return an error from generateKeyPair due to invalid options', () => {
 
         Gen.generateKeyPair({ bits: 1 }, (err, results) => {
 
             expect(err).to.exist();
             expect(results).to.not.exist();
-            done();
         });
     });
 
 
-    it('should return an error from generateKeyPair due to invalid openSSL path', (done) => {
+    it('should return an error from generateKeyPair due to invalid openSSL path', () => {
 
         Gen.setOpenSSLPath('invalid');
-        Gen.generateKeyPair((err, results) => {
+        return new Promise((resolve) => {
 
-            expect(err).to.exist();
-            expect(results).to.not.exist();
-            done();
+            Gen.generateKeyPair((err, results) => {
+
+                expect(err).to.exist();
+                expect(results).to.not.exist();
+                resolve();
+            });
         });
     });
 
-    it('should generate a private/public key pair with default options', (done) => {
+    it('should generate a private/public key pair with default options', () => {
 
         Gen.setOpenSSLPath('openssl');
-        Gen.generateKeyPair((err, results) => {
+        return new Promise((resolve) => {
 
-            expect(err).to.not.exist();
-            expect(results.private).to.be.a.string();
-            expect(results.public).to.be.a.string();
-            done();
+            Gen.generateKeyPair((err, results) => {
+
+                expect(err).to.not.exist();
+                expect(results.private).to.be.a.string();
+                expect(results.public).to.be.a.string();
+                resolve();
+            });
         });
     });
 
-    it('should generate a private/public key pair with user options', (done) => {
+    it('should generate a private/public key pair with user options', () => {
 
         const options = {
             bits: 4096,
             cipher: 'camellia256'
         };
 
-        Gen.generateKeyPair(options, (err, results) => {
+        return new Promise((resolve) => {
 
-            expect(err).to.not.exist();
-            expect(results.private).to.be.a.string();
-            expect(results.public).to.be.a.string();
-            done();
+            Gen.generateKeyPair(options, (err, results) => {
+
+                expect(err).to.not.exist();
+                expect(results.private).to.be.a.string();
+                expect(results.public).to.be.a.string();
+                resolve();
+            });
         });
     });
 
-    it('should throw an error when generateCertificate function has no callback', (done) => {
+    it('should throw an error when generateCertificate function has no callback', () => {
 
         expect(() => Gen.generateCertificate()).throws(Error);
-        done();
     });
 
-    it('should return an error from generateCertificate due to invalid options', (done) => {
+    it('should return an error from generateCertificate due to invalid options', () => {
 
-        Gen.generateCertificate({ bits: 1 }, (err, results) => {
+        return new Promise((resolve) => {
 
-            expect(err).to.exist();
-            expect(results).to.not.exist();
-            done();
+            Gen.generateCertificate({ bits: 1 }, (err, results) => {
+
+                expect(err).to.exist();
+                expect(results).to.not.exist();
+                resolve();
+            });
         });
     });
 
-    it('should return an error from generateCertificate due to invalid openSSL path', (done) => {
+    it('should return an error from generateCertificate due to invalid openSSL path', () => {
 
         Gen.setOpenSSLPath('invalid');
-        Gen.generateCertificate((err, results) => {
+        return new Promise((resolve) => {
 
-            expect(err).to.exist();
-            expect(results).to.not.exist();
-            done();
+            Gen.generateCertificate((err, results) => {
+
+                expect(err).to.exist();
+                expect(results).to.not.exist();
+                resolve();
+            });
         });
     });
 
-    it('should generate a certificate with default options', (done) => {
+
+    it('should generate a certificate with default options', () => {
 
         Gen.setOpenSSLPath('openssl');
-        Gen.generateCertificate((err, results) => {
+        return new Promise((resolve) => {
 
-            expect(err).to.not.exist();
-            expect(results.clientKey).to.be.a.string();
-            expect(results.certificate).to.be.a.string();
-            expect(results.csr).to.be.a.string();
-            expect(results.serviceKey).to.be.a.string();
-            done();
+            Gen.generateCertificate((err, results) => {
+
+                expect(err).to.not.exist();
+                expect(results.clientKey).to.be.a.string();
+                expect(results.certificate).to.be.a.string();
+                expect(results.csr).to.be.a.string();
+                expect(results.serviceKey).to.be.a.string();
+                resolve();
+            });
         });
     });
 
 
-    it('should generate a certificate with user options', (done) => {
+    it('should generate a certificate with user options', () => {
 
         const options = {
             selfSigned: false,
             bits: 1024
         };
 
-        Gen.generateCertificate(options, (err, results) => {
+        return new Promise((resolve) => {
 
-            expect(err).to.not.exist();
-            expect(results.clientKey).to.be.a.string();
-            expect(results.certificate).to.be.a.string();
-            expect(results.csr).to.be.a.string();
-            expect(results.serviceKey).to.be.a.string();
-            done();
+            Gen.generateCertificate(options, (err, results) => {
+
+                expect(err).to.not.exist();
+                expect(results.clientKey).to.be.a.string();
+                expect(results.certificate).to.be.a.string();
+                expect(results.csr).to.be.a.string();
+                expect(results.serviceKey).to.be.a.string();
+                resolve();
+            });
         });
     });
 
